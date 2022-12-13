0ebfccb2e7e4c1b90ffe320dec60974fbe124957
teja-2123 <73219745+teja-2123@users.noreply.github.com>
2022-12-13 16:38:11 +0530

    Update UserDAO.java

--- src/main/java/com/usermanagement/dao/UserDAO.java
+++ src/main/java/com/usermanagement/dao/UserDAO.java
@@ -36,7 +36,7 @@ public class UserDAO {
             String fileSeparator = System.getProperty("file.separator");
 
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection("database.cq7pegotttdz.ap-south-1.rds.amazonaws.com:3306/emp?useSSL=false","admin","admin123");
+			connection = DriverManager.getConnection("jdbc:mysql://database.cq7pegotttdz.ap-south-1.rds.amazonaws.com:3306/emp?useSSL=false","admin","admin123");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
